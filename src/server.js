567f54a937d940c1ad5d2b84d38377baeb482c86
Priyaranjan <priyaranjanved@example.com>
2025-11-29 12:46:16 +0530

    Simplify createBusiness controller

--- src/server.js
+++ src/server.js
@@ -1,18 +1,18 @@
-import dotenv from "dotenv";
-import app from "./app.js";
-import connectDB from "./config/db.js";
+  import dotenv from "dotenv";
+  import app from "./app.js";
+  import connectDB from "./config/db.js";
 
-dotenv.config();
+  dotenv.config();
 
-// Connect MongoDB
-connectDB();
+  // Connect MongoDB
+  connectDB();
 
-const PORT = process.env.PORT || 8080;
+  const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, "0.0.0.0", () => {
-  console.log(`✅ Server running on http://192.168.31.162:${PORT}`);
-  // console.log(`✅ Server running on http://192.168.29.90:8080:${PORT}`);
-});
-// app.listen(PORT, () => {
-//   console.log(`✅ Server running on http://localhost:${PORT}`);
-// });
+  app.listen(PORT, "0.0.0.0", () => {
+    console.log(`✅ Server running on http://192.168.31.162:${PORT}`);
+    // console.log(`✅ Server running on http://192.168.29.90:8080:${PORT}`);
+  });
+  // app.listen(PORT, () => {
+  //   console.log(`✅ Server running on http://localhost:${PORT}`);
+  // });
